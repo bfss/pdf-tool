723a58be95b0696ac35ee7d178e8b007810a50a3
BeifangSusu <BeifangSusu@bfss.com>
2022-03-13 20:43:35 +0800

    code review

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
-
 from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from PySide2.QtCore import Slot
 from PySide2.QtGui import QIcon
-
 from qt_material import apply_stylesheet
-
 from ui.ui_mainwindow import Ui_MainWindow
-
 from thread.CombinePDFThread import CombinePDFThread
 
 
@@ -21,50 +18,59 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowIcon(QIcon('y12.ico'))
 
-        self.pdf_dir = None
-        self.output_dir = None
-
         self.ui.pushButton_select_pdf_dir.clicked.connect(self.select_pdf_dir)
-        self.ui.pushButton_select_output.clicked.connect(
-            self.select_output_dir)
+        self.ui.pushButton_select_output.clicked.connect(self.select_output_dir)
         self.ui.pushButton_combine_pdf.clicked.connect(self.combine_pdf)
 
         self.ui.action_about.triggered.connect(self.about)
 
-        self.ui.lineEdit_select_pdf_dir.setEnabled(False)
-        self.ui.lineEdit_select_output.setEnabled(False)
+    def enable_widgets(self, flag):
+        """启用组件"""
+        if flag:
+            self.ui.lineEdit_select_output.setEnabled(True)
+            self.ui.lineEdit_select_pdf_dir.setEnabled(True)
+            self.ui.pushButton_select_pdf_dir.setEnabled(True)
+            self.ui.pushButton_select_output.setEnabled(True)
+            self.ui.pushButton_combine_pdf.setEnabled(True)
+        else:
+            self.ui.lineEdit_select_output.setEnabled(False)
+            self.ui.lineEdit_select_pdf_dir.setEnabled(False)
+            self.ui.pushButton_select_pdf_dir.setEnabled(False)
+            self.ui.pushButton_select_output.setEnabled(False)
+            self.ui.pushButton_combine_pdf.setEnabled(False)
 
     @Slot()
     def select_pdf_dir(self):
         """选择PDF文件夹"""
-        self.pdf_dir = QFileDialog.getExistingDirectory(self)
-        self.ui.lineEdit_select_pdf_dir.setText(self.pdf_dir)
+        selected_dir = QFileDialog.getExistingDirectory(self)
+        if selected_dir:
+            self.ui.lineEdit_select_pdf_dir.setText(selected_dir)
 
     @Slot()
     def select_output_dir(self):
         """选择输出文件夹"""
-        self.output_dir = QFileDialog.getExistingDirectory(self)
-        self.ui.lineEdit_select_output.setText(self.output_dir)
+        selected_dir = QFileDialog.getExistingDirectory(self)
+        if selected_dir:
+            self.ui.lineEdit_select_output.setText(selected_dir)
 
     @Slot()
     def combine_pdf(self):
         """合并pdf"""
-        if not self.pdf_dir or not self.output_dir:
+        pdf_dir = self.ui.lineEdit_select_pdf_dir.text().strip()
+        output_dir = self.ui.lineEdit_select_output.text().strip()
+        if os.path.isdir(pdf_dir) and os.path.isdir(output_dir):
+            self.enable_widgets(False)
+            self.combine_pdf_thread = CombinePDFThread(
+                pdf_dir, output_dir)
+            self.combine_pdf_thread.finish_signal.connect(
+                self.combine_pdf_thread_finished)
+            self.combine_pdf_thread.start()
+        else:
             QMessageBox.critical(
                 self,
                 "错误",
-                "出错啦"
+                "需要选择有效的文件夹"
             )
-            return
-        self.ui.pushButton_select_pdf_dir.setEnabled(False)
-        self.ui.pushButton_select_output.setEnabled(False)
-        self.ui.pushButton_combine_pdf.setEnabled(False)
-
-        self.combine_pdf_thread = CombinePDFThread(
-            self.pdf_dir, self.output_dir)
-        self.combine_pdf_thread.finish_signal.connect(
-            self.combine_pdf_thread_finished)
-        self.combine_pdf_thread.start()
 
     @Slot(int)
     def combine_pdf_thread_finished(self, flag):
@@ -88,9 +94,7 @@ class MainWindow(QMainWindow):
                 "错误",
                 "合并完毕，下列这些PDF存在问题，没有被合并：\n" + bad_pdfs_str
             )
-        self.ui.pushButton_select_pdf_dir.setEnabled(True)
-        self.ui.pushButton_select_output.setEnabled(True)
-        self.ui.pushButton_combine_pdf.setEnabled(True)
+        self.enable_widgets(True)
 
     def about(self):
         """关于"""
--- thread/CombinePDFThread.py
+++ thread/CombinePDFThread.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-import PyPDF2
-from PyPDF2 import PdfFileMerger
-
-from PySide2.QtCore import QThread, Signal
-
 import os
 from datetime import datetime
+from PyPDF2 import PdfFileMerger
+from PySide2.QtCore import QThread, Signal
 
 
 class CombinePDFThread(QThread):
@@ -24,9 +21,9 @@ class CombinePDFThread(QThread):
     def run(self):
         pdf_count = 0
         merger = PdfFileMerger()
-        for root, dirs, files in os.walk(self.pdf_dir):
+        for root, _, files in os.walk(self.pdf_dir):
             for f in files:
-                filename, file_extension = os.path.splitext(f)
+                _, file_extension = os.path.splitext(f)
                 if file_extension.lower() == '.pdf':
                     pdf = os.path.join(root, f)
                     try:
@@ -39,7 +36,7 @@ class CombinePDFThread(QThread):
             self.finish_signal.emit(-1)
             return
         else:
-            combined_pdf_name = datetime.utcnow().strftime("%Y_%m_%d_%H_%M_%S")
+            combined_pdf_name = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
             output_file = os.path.join(self.output_dir, combined_pdf_name + '.pdf')
             merger.write(output_file)
             merger.close()
